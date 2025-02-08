0b6a292ea663434df1b0c540916efc2ef946b07f
Varun Gotmare <varunpgotmare@gmail.com>
2025-02-08 20:04:51 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div className='items-center justify-center flex flex-col min-h-screen bg-gray-100'>
+        <h1 className='text-4xl font-bold text-center'>Counter App</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
